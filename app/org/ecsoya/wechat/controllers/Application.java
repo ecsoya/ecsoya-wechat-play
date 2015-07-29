0bfec31d3ae8fca70dd7db860872f620f07f24dc
Ecsoya <jin.liu@soyatec.com>
2015-07-29 12:34:37 +0800

    Back Application

--- app/org/ecsoya/wechat/controllers/Application.java
+++ app/org/ecsoya/wechat/controllers/Application.java
@@ -1,31 +1,38 @@
 package org.ecsoya.wechat.controllers;
 
-import org.ecsoya.wechat.auth.AuthenticateRequest;
 import org.ecsoya.wechat.utils.EcsoyaWeChat;
+import org.ecsoya.wechat.utils.SHA1Utils;
 
 import play.mvc.Controller;
 import play.mvc.Http.Request;
 import play.mvc.Result;
 
-import com.qq.weixin.mp.aes.AesException;
-
 public class Application extends Controller {
 
-	@AuthenticateRequest
 	public static Result index() {
 		Request request = request();
+
 		String signature = request.getQueryString("signature");
 		String timestamp = request.getQueryString("timestamp");
 		String nonce = request.getQueryString("nonce");
 		String echostr = request.getQueryString("echostr");
-		if (echostr != null) {
+
+		if (timestamp != null && nonce != null) {
 			try {
-				return ok(EcsoyaWeChat.getCrypt().verifyUrl(signature,
-						timestamp, nonce, echostr));
-			} catch (AesException e) {
+				String sign = SHA1Utils.getSHA1Sign(EcsoyaWeChat.TOKEN,
+						timestamp, nonce);
+				if (sign != null && sign.equals(signature)) {
+					return ok(echostr);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 				return badRequest(e.getMessage());
 			}
 		}
+		if (echostr != null) {
+			return ok(echostr);
+		}
+
 		return ok("Invalid Token");
 	}
 
